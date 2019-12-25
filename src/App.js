8767e406c65172a5cd1930b9229614cecb8da75f
Jimmy <bruno_pier@hotmail.com>
2019-12-25 14:43:32 -0300

    Adding components & insert in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Suscriptores from './componentes/suscriptores/Suscriptores';
+import MostrarSuscriptor from './componentes/suscriptores/MostrarSuscriptor';
+import EditarSuscriptor from './componentes/suscriptores/EditarSuscriptor';
+import NuevoSuscriptor from './componentes/suscriptores/NuevoSuscriptor';
+
 
 
 function App() {
   return (
-    <h1>BiblioStore</h1>
-  );
+      <Router>
+        <Switch>
+          <Route exact path="/suscriptores" component={Suscriptores} />
+          <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+          <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+          <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
+        </Switch>
+      </Router>
+    );
 }
 
 export default App;
