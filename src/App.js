1534aba3dde647991bbe4f431243cd24f1bdf075
Jimmy <bruno_pier@hotmail.com>
2019-12-25 20:53:25 -0300

    Adding store & provider in to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import store from './store';
+import { Provider } from 'react-redux';
 
 import Suscriptores from './componentes/suscriptores/Suscriptores';
 import MostrarSuscriptor from './componentes/suscriptores/MostrarSuscriptor';
@@ -10,17 +12,19 @@ import Navbar from './componentes/layout/Navbar';
 
 function App() {
   return (
-      <Router>
-          <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/suscriptores" component={Suscriptores} />
-                <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
-                <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
-                <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
-              </Switch>
-            </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+            <Navbar />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/suscriptores" component={Suscriptores} />
+                  <Route exact path="/suscriptores/nuevo" component={NuevoSuscriptor} />
+                  <Route exact path="/suscriptores/mostrar/:id" component={MostrarSuscriptor} />
+                  <Route exact path="/suscriptores/editar/:id" component={EditarSuscriptor} />
+                </Switch>
+              </div>
+        </Router>
+      </Provider>
     );
 }
 
